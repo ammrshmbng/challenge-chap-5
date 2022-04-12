8c85a9ba96bc9f5af55ea44bb771ed01d464e2f3
ammrshmbng <ammarmare01@gmail.com>
2022-04-12 15:15:16 +0700

    add all data

---
+++ index.js
@@ -0,0 +1,26 @@
+// ubah index.html menjadi index.ejs lalu pindahkan ke view
+// membuat alamat untuk menampilkan alamatnya
+// untuk assetnya sajikan secara static menggunakan express statis
+
+const express = require('express');
+const app = express();
+
+const port  = 3000;
+
+app.set('view engine','ejs');
+
+// middleware
+app.use('/assets',express.static('assets'))
+
+
+//routing
+app.get('/game',function(req,res){
+    res.render('game');
+});
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+
+app.listen(port,()=>{console.log('server is running')})
